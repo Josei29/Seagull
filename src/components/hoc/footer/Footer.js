445486218a5173b6c607d1227823147d14d2aeb9
Josei29 <jjimenez.r98@gmail.com>
2019-09-25 17:42:20 -0500

    Footer

--- src/components/hoc/footer/Footer.js
+++ src/components/hoc/footer/Footer.js
@@ -4,10 +4,10 @@ import "./Footer.css";
 const Footer = () => {
     return(
         <div className="footer">
-            <div>P.O Box 700996 Dallas, TX 75370</div>
-            <div>seagullconstruction1@gmail.com</div>
-            <div>(214) 622 - 6505</div>
-            <div>&copy; 2019 - Seagull Construction LLC</div>
+            <div className="footer--text">P.O Box 700996 Dallas, TX 75370</div>
+            <div className="footer--text">seagullconstruction1@gmail.com</div>
+            <div className="footer--text">(214) 622 - 2505</div>
+            <div className="footer--text">&copy; 2019 - Seagull Construction LLC</div>
         </div>
     );
 };
